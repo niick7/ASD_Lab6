e233f742908bd243cfb77f56b8df96afac3e0836
Nhan Vo <niick7@gmail.com>
2020-11-04 01:30:50 -0600

    First commit

---
+++ src/main/java/miu/asd/lab6/LogLevel.java
@@ -0,0 +1,5 @@
+package miu.asd.lab6;
+
+public enum LogLevel {
+  DEBUG, ERROR, FATAL, INFO, TRACE, WARNING
+}
---
+++ src/main/java/miu/asd/lab6/Logger.java
@@ -0,0 +1,5 @@
+package miu.asd.lab6;
+
+public interface Logger {
+  void log(LogLevel logLevel, String message);
+}
